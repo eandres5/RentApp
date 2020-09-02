855c58377b2367d3b2c692c05941baa67d435f17
eandres5 <andresalquinga@hotmail.com>
2020-09-02 16:05:42 -0500

    check02092020

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  
+  }
 
   ngOnInit() {
   }
+
 
 }
