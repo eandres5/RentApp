f69e63e3581a90d13553c310938405dd9116c008
DESKTOP-70T0AE3\usuario <bryan.corrales@epn.edu.ec>
2020-10-13 12:12:11 -0500

    Restablecer contra

--- src/app/index/index-routing.module.ts
+++ src/app/index/index-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
       },
       {
         path: 'registro',
-        loadChildren: () => import('../pages/registro/registro.module').then(m => m.RegistroPageModule),canActivate:[RegistroGuard]
+        loadChildren: () => import('../pages/registro/registro.module').then(m => m.RegistroPageModule)
       },
       {
         path: 'politicas',
